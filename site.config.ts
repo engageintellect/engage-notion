7d58094ffefc94f41733644c9dff187815ff1922
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-03-06 22:21:47 -0800

    init3

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from "./lib/site-config";
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageid: "2e801532a8be488b8c4b0d5f1a145a1b",
+  rootNotionPageId: "2e801532a8be488b8c4b0d5f1a145a1b",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
